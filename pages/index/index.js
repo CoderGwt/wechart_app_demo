3a63ba2503a6c2b101b6bc142a4c7699e3b6730a
CoderGwt <1341680843@qq.com>
2018-11-19 19:06:25 +0800

    swiper resource

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,21 @@
 //index.js
 
 Page({
+  // 处理数据
+  data: {
+    "imgUrls": [
+      "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_1.png",
+      "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_2.png",
+      "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_3.png",
+      "https://lg-awf6ud7o-1257084981.cos.ap-shanghai.myqcloud.com/item_4.png",
+    ],
+    "indicatorDots": true,
+    "indicatorColor": "#ccc",
+    "indicatorActiveColor": "#fff",
+    "autoplay": true,
+    "interval": 3000,
+    "duration": 1000,
+    "circular": true
+
+  }
 })
