9b0ffd5ee6276345a520aab8d31925adf1b47820
CoderGwt <1341680843@qq.com>
2018-11-25 11:27:19 +0800

    add bind event

--- pages/data/data.js
+++ pages/data/data.js
@@ -5,8 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+      value: "",
+      msg: ""
   },
+
+  // view 文字点击事件 click
+  click: function(){
+    console.log("bind:tap 点击")
+  },
+
+  click2: function(){
+    console.log("bindtap点击")
+  },
+
+  bindInput: function(ev){
+    this.setData({
+      msg: ev.detail.value
+    })
+  },
+
+  getFocus: function(){
+    console.log("输入框获取到了焦点")
+  },
+
+  lostFocus: function(ev){
+    console.log("输入框失去了焦点");
+    console.log(ev)
+    this.setData({
+      value: ev.detail.value
+    })
+  },
+
+
 
   /**
    * 生命周期函数--监听页面加载
