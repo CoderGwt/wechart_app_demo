c7eb58e0b58fadbab1d137b980e47fe636cbd0a6
CoderGwt <1341680843@qq.com>
2019-03-22 11:38:36 +0800

    事件 review

--- pages/review/review.js
+++ pages/review/review.js
@@ -11,6 +11,21 @@ Page({
     duration: 1000,
     indicatorColor: "#fff",
     indicatorActiveColor: "#00f",
-    circular: true
+    circular: true,
+
+    name: "python",
+    age: 23,
+    cities: [
+      "武当山", "少林寺", "内蒙古", "丽江古城"
+    ],
+    score: 92
   },
+  youxiu: function(res) {
+    wx.showToast({
+      title: '恭喜晋级',
+    });
+    console.log(res);
+    console.log(res.currentTarget.dataset.index);
+    console.log(res.target.dataset.index);
+  }
 })
